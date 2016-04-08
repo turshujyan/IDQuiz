85f830c191620e867eeee49e72aecf146e74665c
Hermine <hermine.turshujyan@gmail.com>
2016-04-08 16:40:57 +0400

    Initial Commit

---
+++ IDQuiz/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  IDQuiz
+//
+//  Created by Hermine on 4/8/16.
+//  Copyright © 2016 Hermine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ IDQuiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IDQuiz
+//
+//  Created by Hermine on 4/8/16.
+//  Copyright © 2016 Hermine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
